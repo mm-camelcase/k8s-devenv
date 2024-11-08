ae45af0f531fdd4374eac13abc2ef061be033612
Mark Mitchell <markmitchell451@gmail.com>
2024-11-08 09:16:43 +0000

    remove nginx-hello

--- scripts/install-services.sh
+++ scripts/install-services.sh
@@ -3,10 +3,10 @@
 # image in public repo
 helm upgrade --install -n devenv user-service oci://ghcr.io/mm-camelcase/helm-charts/user-service --devel -f "$RESOURCES_DIR/devenv-values.yaml"
 # image in private repo
-helm upgrade --install -n devenv nginx-hello oci://ghcr.io/mm-camelcase/helm-charts/nginx-hello --devel
+#helm upgrade --install -n devenv nginx-hello oci://ghcr.io/mm-camelcase/helm-charts/nginx-hello --devel
 
 # Validate all charts
 kubectl -n devenv rollout status deployment/user-service
-kubectl -n devenv rollout status deployment/nginx-hello
+#kubectl -n devenv rollout status deployment/nginx-hello
 
 
